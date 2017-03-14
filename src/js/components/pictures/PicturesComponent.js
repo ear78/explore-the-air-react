b5704cb0b8991c4c546b82dc843c2914cae5fabf
elliot richardson <ear78@aol.com>
2017-03-14 11:38:52 -0500

    added images for pictures page and css

--- src/js/components/pictures/PicturesComponent.js
+++ src/js/components/pictures/PicturesComponent.js
@@ -8,7 +8,12 @@ export default class Pictures extends React.Component {
             <div class="pictures">
                 <h1>ETA Pictures</h1>
                 <h5>Picture Perfection</h5>
-                <p>Four loko eu butcher paleo, post-ironic synth esse heirloom lomo slow-carb. DIY meggings sustainable, pickled godard hashtag kinfolk microdosing ullamco nihil biodiesel velit. Hella dreamcatcher occupy excepteur, meggings live-edge chicharrones. Sint seitan mixtape, you probably havent heard of them health goth knausgaard etsy. Nostrud gluten-free meditation fingerstache. Kale chips pop-up pariatur kickstarter offal, neutra 3 wolf moon meh chicharrones eiusmod. Hoodie williamsburg wolf umami enim.</p>
+                <p>
+                    <img src="../img/paul-morris-mtn.jpg" alt="mountains"/>
+                    <img src="../img/paul-morris-mtn.jpg" alt="mountains"/>
+                    <img src="../img/paul-morris-mtn.jpg" alt="mountains"/>
+                    <img src="../img/paul-morris-mtn.jpg" alt="mountains"/>
+                </p>
             </div>
         )
     }
