f69900b112584c98d91caab35fb1537b7c07fa65
elliot richardson <ear78@aol.com>
2017-03-13 22:12:46 -0500

    added css loader into webpack and basic css files

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Link} from "react-router";
 
 import Footer from "../components/Footer";
-import Header from "../components/Header";
+import Header from "../components/header/Header";
 
 export default class Layout extends React.Component {
     navigate(){
