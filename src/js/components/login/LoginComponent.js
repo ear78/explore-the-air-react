7b5772fc848547147e2a01de15b2be665a4a119b
elliot richardson <ear78@aol.com>
2017-03-16 23:56:30 -0500

    functionality is working for login submit form

--- src/js/components/login/LoginComponent.js
+++ src/js/components/login/LoginComponent.js
@@ -8,27 +8,36 @@ export default class LoginComponent extends React.Component {
     constructor(){
         super();
         this.state = {
-            name: "",
-            email: "",
+            username: '',
+            password: ''
         }
     }
 
     onChange(e){
-        console.log(e.target.value);
         this.setState({
             [e.target.name]: e.target.value
         })
     }
-    
+
+    onSubmit(e){
+        e.preventDefault();
+        console.log(this.state);
+        alert('this was submitted! Thank you!')
+    }
+
     render() {
         return (
             <div class="login-container">
                 <h1>Login To ETA</h1>
                 <h5>Track Your Adventures</h5>
                 <div class="login-background">
-                    <form>
-                    <input type="text" placeholder="User Name" name="username" onChange={this.onChange.bind(this)}/>
-                    <input type="password" placeholder="Password" password="password"/>
+                    <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type="text" placeholder="User Name" value={this.state.username}
+                    onChange={this.onChange.bind(this)}
+                    name="username" />
+                    <input type="password" placeholder="Password"
+                    value={this.state.password}
+                    onChange={this.onChange.bind(this)} name="password"/>
                     <p>Have An Account? Sign Up Below!</p>
                     <button type="submit">Login</button>
                     <SignUpButtonComponent />
