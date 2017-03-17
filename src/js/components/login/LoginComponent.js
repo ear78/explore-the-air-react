028582505dfc46360d437091a6016ddc320737d4
elliot richardson <ear78@aol.com>
2017-03-16 22:55:02 -0500

    adding functionality for login account, setting up form and on change handlers

--- src/js/components/login/LoginComponent.js
+++ src/js/components/login/LoginComponent.js
@@ -5,17 +5,33 @@ import "./logincomponent.css";
 import SignUpButtonComponent from "../signupbuttoncomponent/SignUpButtonComponent.js";
 
 export default class LoginComponent extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            name: "",
+            email: "",
+        }
+    }
+
+    onChange(e){
+        console.log(e.target.value);
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
     render() {
         return (
             <div class="login-container">
                 <h1>Login To ETA</h1>
                 <h5>Track Your Adventures</h5>
                 <div class="login-background">
-                    <input type="text" placeholder="Name"/>
-                    <input type="email" placeholder="Email"/>
+                    <form>
+                    <input type="text" placeholder="User Name" name="username" onChange={this.onChange.bind(this)}/>
+                    <input type="password" placeholder="Password" password="password"/>
                     <p>Have An Account? Sign Up Below!</p>
                     <button type="submit">Login</button>
                     <SignUpButtonComponent />
+                    </form>
                 </div>
 
             </div>
